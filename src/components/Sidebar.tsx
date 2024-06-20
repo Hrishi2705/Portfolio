b2e0c60f8a4389cabe558f218d69e0b196250656
Hrishikesh <hrishikesh.sam09@gmail.com>
2024-06-20 15:20:32 +0530

    resume link update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -36,7 +36,7 @@ export const Sidebar = () => {
               <SidebarHeader />
               <Navigation setOpen={setOpen} />
             </div>
-            <Badge href="https://drive.google.com/file/d/1FXLwttCZj-MVzpBRVZ11Rw9G58f3C-F3/view?usp=sharing" text="Read Resume"/>
+            <Badge href="https://drive.google.com/file/d/1K_32ExXOa0-R2pyuylJc21OwwLQOh-iW/view" text="Read Resume"/>
           </motion.div>
         )}
       </AnimatePresence>
